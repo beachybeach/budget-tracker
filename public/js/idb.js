33d466116d35f5d0da83c3526792c6705bc177d7
Riley <rsbeach97@gmail.com>
2021-02-23 17:35:21 -0700

    data being saved to L.S. need to save upon refresh

--- public/js/idb.js
+++ public/js/idb.js
@@ -26,3 +26,27 @@ request.onerror = function(event) {
   // log error here
   console.log(event.target.errorCode);
 };
+
+// This function will be executed if we attempt to submit a new pizza and there's no internet connection
+function saveRecord(record) {
+  // open a new transaction with the database with read and write permissions 
+  const transaction = db.transaction(['new_budget'], 'readwrite');
+
+  // access the object store for `new_budget`
+  const budgetObjectStore = transaction.objectStore('new_budget');
+
+  // add record to your store with add method
+  budgetObjectStore.add(record);
+}
+
+function uploadBudget() {
+  // open a transaction on your db
+  const transaction = db.transaction(['new_budget'], 'readwrite');
+
+  // access your object store
+  const budgetObjectStore = transaction.objectStore('new_budget');
+
+  // get all records from store and set to a variable
+  const getAll = budgetObjectStore.getAll();
+
+}
